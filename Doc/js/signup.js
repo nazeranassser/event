c154c8f0a194dcbce98a8da402e88340de45b91f
Mousa-alaaldeen <azmimousa396@gmail.com>
2024-10-04 17:38:34 +0300

    update header

--- Doc/js/signup.js
+++ Doc/js/signup.js
@@ -1,25 +1,43 @@
+// logout.js
+
 window.onload = function() {
     var isLoggedIn = localStorage.getItem("isLoggedIn");
     var logoutBtn = document.getElementById("logoutBtn");
     var bookNow = document.getElementById("BookNow");
     
-    if (isLoggedIn === "true") {
-        logoutBtn.style.display = "inline-block"; 
-        bookNow.style.display = "none"; 
-    } else {
-        logoutBtn.style.display = "none"; 
-        bookNow.style.display = "inline-block"; 
-    }
-    
-    logoutBtn.onclick = function() {
-        var confirmLogout = confirm("Are you sure you want to log out?");
-        if (confirmLogout) {
-            localStorage.setItem("isLoggedIn", "false"); 
-            localStorage.removeItem("userInfo");  
-        
-            window.location.href = "index.html";  
+    // Function to update button visibility based on login state
+    function updateButtonVisibility() {
+        if (isLoggedIn === "true") {
+            logoutBtn.style.display = "inline-block"; 
+            bookNow.style.display = "none"; 
         } else {
-            return false;
+            logoutBtn.style.display = "none"; 
+            bookNow.style.display = "inline-block"; 
         }
+    }
+
+    updateButtonVisibility();
+
+    // Event listener for Logout button to show the modal
+    logoutBtn.onclick = function(event) {
+        event.preventDefault(); // Prevent default action
+        var logoutModal = new bootstrap.Modal(document.getElementById('logoutModal'));
+        logoutModal.show();
+    };
+
+    // Event listener for Confirm Logout button in the modal
+    var confirmLogoutBtn = document.getElementById("confirmLogout");
+    confirmLogoutBtn.onclick = function() {
+        // Perform logout actions
+        localStorage.setItem("isLoggedIn", "false"); 
+        localStorage.removeItem("userInfo");  
+        
+        // Optionally, hide the Logout button and show the Book Now button
+        updateButtonVisibility();
+        
+        // Redirect to homepage after a short delay to allow UI update
+        setTimeout(function() {
+            window.location.href = "index.html";  
+        }, 500);
     };
 };
